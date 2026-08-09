f2080298d204f1322c9abd6cd2d0e884a6bae422
CarrotCrusher5571 <ConnorVuongMinhNguyen@gmail.com>
2026-08-09 16:45:23 +1200

    parental controls working

--- ATTuneProject/Scenes/parental_control_pop_up.gd
+++ ATTuneProject/Scenes/parental_control_pop_up.gd
@@ -1,16 +1,6 @@
 extends Control
 var text
 var password = "password"
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_line_edit_text_changed(new_text: String) -> void:
 	text = new_text;
 
@@ -22,6 +12,6 @@ func _on_exit_button_down() -> void:
 func _on_enter_parent_mode_button_down() -> void:
 	if(text == password):
 		print("Password Accepted")
-		pass #Stuff I guess
+		get_tree().change_scene_to_file("res://Scenes/parental_controls_home.tscn")
 	else:
 		print("Password Incorrect")
